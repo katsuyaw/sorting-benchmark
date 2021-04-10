d1bbc0d9313ac50ce2f4611945734f65e2401262
katsuyaw <kw@kmail.me>
2021-04-10 15:47:33 -0700

    Create array randomizer

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -89,25 +89,41 @@ void printBenchmarks(int* size, int i){
               << heapTime(input)    << std::setw(15) << quickTime(input)      << std::setw(15) << mergeTime(input) <<std::endl;
 }
 
-int* IArray;
-int* SArray;
-int* BArray;
-int* HArray;
-int* QArray;
-int* Marray;
+int* iArray;
+int* sArray;
+int* bArray;
+int* hArray;
+int* qArray;
+int* mArray;
 
 
 
 int main() {
     int size[] = {100000, 200000, 300000, 400000, 500000};
     int len = sizeof(size)/sizeof(size[0]);
-
-    int randomVal;
     srand(time(0));
 
 
     printBenchmarkTable();
     for(int i=0; i<len; i++){
+        iArray = new int[size[i]];
+        sArray = new int[size[i]];
+        bArray = new int[size[i]];
+        hArray = new int[size[i]];
+        qArray = new int[size[i]];
+        mArray = new int[size[i]];
+
+        for(int j=0; j<size[i]; j++){
+            int randomVal = rand() % 100;
+            iArray[j] = randomVal;
+            sArray[j] = randomVal;
+            bArray[j] = randomVal;
+            hArray[j] = randomVal;
+            qArray[j] = randomVal;
+            mArray[j] = randomVal;
+        }
+        
+        
 
         printBenchmarks(size, i);
     }
